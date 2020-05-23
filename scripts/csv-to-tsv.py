8dda242496d1148d6bc36a3cfbde065e536ed809
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2020-05-23 18:07:09 -0500

    adjust conversion script

--- scripts/csv-to-tsv.py
+++ scripts/csv-to-tsv.py
@@ -1,6 +1,6 @@
 import csv
  
-with open("./public/latest.csv",'rt') as csvin, open("./public/latest.tsv", 'w', newline='\n') as tsvout:
+with open("./public/latest.csv",'rt') as csvin, open("./public/latest.tsv", 'w') as tsvout:
     tsvout = csv.writer(tsvout, delimiter='\t')
     for row in csv.reader(csvin):
         tsvout.writerow([x.replace('\n', ' ') for x in row])
