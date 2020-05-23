4ab121e42eaaeb9bf18be1a3b46d80014f439fc2
Andrew Michael McNutt <mcnutt.andrew@gmail.com>
2020-05-23 18:13:50 -0500

    more build tweaks

--- scripts/get-latest.sh
+++ scripts/get-latest.sh
@@ -1,3 +1,4 @@
 touch public/latestX.csv
 curl https://docs.google.com/spreadsheets/d/1wZhPLMCHKJvwOkP4juclhjFgqIY8fQFMemwKL2c64vk/gviz/tq?tqx=out:csv > public/latest.csv
 python scripts/csv-to-tsv.py
+cp public/latest.tsv build/
